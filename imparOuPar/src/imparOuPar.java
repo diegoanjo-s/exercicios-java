7b9e788bfad0ea55ca4945e1ff07bd0bb503337e
Diego Anjos Aragão <diegoanjosaragao1712@gmail.com>
2026-06-11 17:33:12 -0300

    atualização de codigo exercicio3

--- imparOuPar/src/imparOuPar.java
+++ imparOuPar/src/imparOuPar.java
@@ -1,4 +1,36 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
 public class imparOuPar {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Digite o primeiro número: ");
+        int num1 = scanner.nextInt();
+
+        int num2;
+        do {
+            System.out.println("Digite o segundo número maior que o primeiro: ");
+            num2 = scanner.nextInt();
+
+            if (num2 <= num1) {
+                System.out.println("Número inválido! O segundo número deve ser maior que " + num1 + ". Tente novamente.");
+            }
+        } while (num2 <= num1);
+
+        System.out.println("Digite 1 para Par ou 2 para Ímpar: ");
+        int opcao = scanner.nextInt();
+
+        for (int i = num1; i <= num2; i++) {
+
+            if (opcao == 1 && i % 2 == 0) {
+                System.out.println(i);
+            }
+            else if (opcao == 2 && i % 2 != 0) {
+                System.out.println(i);
+            }
+
+        }
+
+        scanner.close();
+    }
 }
